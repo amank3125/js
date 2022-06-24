de571a1f0c5e0a96b3e6b17d6c3e6cb199a29197
amank3125 <amank3125@gmail.com>
2022-06-24 23:06:45 +0530

    commit

--- test.js
+++ test.js
@@ -2,4 +2,6 @@ console.log("it's working");
 
 console.log("I've just enabled auto sync after commit");
 
-console.log('this is me changing this file from another machine')
+console.log('this is me changing this file from another machine');
+
+console.log('so i will have to pull also in order to sync properly')
